8acbcf6cfc54749dfcb38f6197f4cf0e65888464
Benwaf <benwafriley@gmail.com>
2022-08-10 19:38:00 -0700

    corrections

--- bettypride.c
+++ bettypride.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- *main - Entry point
- *
- *Return: 0
- */
+*main - Entry point
+*
+*Return: 0
+*/
+
 int main(void)
 {
-	printf("A shell provides you with an interface ");
-	printf("to the Unix operating system.\n");
-	printf("Therefore, a shell is basically an environment ");
-	printf("in which we run our commands, programs and shell scripts.\n");
-
-	return (0);
+printf("A shell provides you with an interface ");
+printf("to the Unix operating system.\n");
+printf("Therefore, a shell is basically an environment ");
+printf("in which we run our commands, programs and shell scripts.\n");
+return (0);
 }
